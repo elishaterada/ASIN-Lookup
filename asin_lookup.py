5d8fc01d4b3b80d4744ccb31917874f520dbc8e1
Elisha Terada <elishaterada@gmail.com>
2013-06-12 18:46:21 -0700

    Initial commit

---
+++ asin_lookup.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+
+"""
+ASIN Lookup on Amazon.com
+"""
+
+import sys
+import urllib2
+import copy
+from bs4 import BeautifulSoup
+
+# Query ASIN on Amazon
+
+
+def asin_lookup(movie_title):
+
+
+    # Convert spaces to pluses
+    query_movie_title = movie_title.replace(' ', '+')
+
+    # Query the movie title on Amazon.com
+    asin_query_url = "http://www.amazon.com/s/?url=search-alias%3Daps&field-keywords=" + query_movie_title + "&rh=i%3Aaps%2Ck%3A" + query_movie_title
+
+    try:
+        soup = BeautifulSoup(urllib2.urlopen(asin_query_url))
+    except urllib2.HTTPError:
+        print "URL could not be opened"
+        return None
+    else:
+        # Look through first three results
+        for i in range(0, 5):
+
+            query_result = soup.find(id="result_" + str(i))
+
+            # Get ASIN
+            query_result_asin = copy.copy(query_result)
+            matched_asin_string = query_result_asin.get('name')
+
+            # Get Movie Title
+            query_result_title = copy.copy(query_result)
+            matched_movie_title = query_result_title.find("div", attrs={'class': 'productTitle'}).get_text()
+
+            print matched_asin_string + " - " + matched_movie_title + "\n"
+
+    return
+
+
+def main(argv=None):
+    if argv is None:
+        argv = sys.argv
+
+    #0 Parse options and user input
+    if len(argv) < 2:
+        print "Please enter a movie title to lookup"
+        exit()
+
+    movie_title = argv[1]
+
+    #1 Query for keywords
+    asin_lookup(movie_title)
+
+if __name__ == "__main__":
+    sys.exit(main())
